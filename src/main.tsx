8ca1bd379b9c9e595e40b237d9eb9a3519e73bbe
Liam Hales <liam.hales96@gmail.com>
2026-04-03 00:34:18 +0100

    Added `main.tsx` entry point file used to render the `App` component

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import { render } from 'ink';
+import { App } from './components/index.js';
+
+/**
+ * The main entry point to the app which renders the `App`
+ * component using the `render` function from `ink`
+ */
+render(<App />);
