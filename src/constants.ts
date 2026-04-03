0a4f3b84af80df76038c77e49cdb03753cd95553
Liam Hales <liam.hales96@gmail.com>
2026-04-03 14:42:30 +0100

    Added `commands` constant which describes all possible CLI commands the user can execute

--- src/constants.ts
+++ src/constants.ts
@@ -8,3 +8,25 @@ export const colours = {
   lightGrey: '#c7c7c7',
   darkGrey: '#4a4a4a',
 } as const;
+
+/**
+ * Describes all possible CLI
+ * commands the user can execute
+ */
+export const commands = [
+  {
+    name: '/init <hostname>',
+    description: 'Used to connect to a Homebridge server',
+    run: '/init',
+  },
+  {
+    name: '/login',
+    description: 'Used to login to Homebridge',
+    run: '/login',
+  },
+  {
+    name: '/logout',
+    description: 'Used to logout of Homebridge and clear saved credentials',
+    run: '/logout',
+  },
+] as const;
