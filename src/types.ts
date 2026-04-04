c0fa574d7767b2609036b42aee5c6e727922d25f
Liam Hales <liam.hales96@gmail.com>
2026-04-04 19:48:03 +0100

    Added block types

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,16 @@
+import { Command } from './commands/index.js';
+
 /**
  * Describes the different
  * modes the app can be in
  */
 export type AppMode = 'idle' | 'running';
+
+/**
+ * The union type for
+ * all block types
+ */
+export type Block = ICommandBlock | IErrorBlock;
 
 /**
  * Used to create a map type from a
@@ -20,3 +28,25 @@ export type Map<
     Readonly<Record<U, K>>
   >;
 };
+
+/**
+ * Describes the command block used to
+ * store data about an executed command
+ */
+export interface ICommandBlock {
+  readonly type: 'command';
+  readonly id: string;
+  readonly input: string;
+  readonly commandId: Command['id'];
+}
+
+/**
+ * Describes the error block used to
+ * store data about an execution error
+ */
+export interface IErrorBlock {
+  readonly type: 'error';
+  readonly id: string;
+  readonly input: string;
+  readonly message: string;
+}
