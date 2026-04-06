385f16d505f41999964643f8d0edab8548b011bf
Liam Hales <liam.hales96@gmail.com>
2026-04-06 15:50:42 +0100

    Updated `App` component to use app context and render info components

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,10 +1,16 @@
-import { FunctionComponent, ReactElement, useState } from 'react';
-import { Text, Box, useInput, useApp } from 'ink';
-import { colours } from '../constants.js';
-import { Header, CommandInput, BlockSwitch, Keybindings } from './index.js';
-import { AppMode, Block } from '../types.js';
-import { nanoid } from 'nanoid';
-import { commands } from '../commands/index.js';
+import { FunctionComponent, ReactElement } from 'react';
+import { Box, useInput } from 'ink';
+import { useApp } from '../hooks/index.js';
+import {
+  Header,
+  SetupInfo,
+  ServerInfo,
+  ApiInfo,
+  LoginInfo,
+  CommandInput,
+  BlockSwitch,
+  Keybindings,
+} from './index.js';
 
 /**
  * The root app component used as the
@@ -13,97 +19,31 @@ import { commands } from '../commands/index.js';
  * @returns The `App` component
  */
 const App: FunctionComponent = (): ReactElement => {
-  const { exit } = useApp();
-
-  const [mode, setMode] = useState<AppMode>('idle');
-  const [blocks, setBlocks] = useState<Block[]>([]);
-  const [inputValue, setInputValue] = useState<string>('');
-
-  /**
-   * Used to validate and
-   * execute a given input
-   *
-   * @param input The input
-   */
-  const _executeInput = (input: string): void => {
-    const blockId = nanoid(16);
-
-    // Check if the user
-    // wants to exit
-    if (input === 'exit') {
-      exit();
-    }
-
-    try {
-      // Attempt to find a command that matches
-      // the users input via its run value
-      const command = commands.find((command) => command.run === input);
-
-      // If no command could be found
-      // then throw an error
-      if (command == null) {
-        throw new Error(`Command "${input}" not found`);
-      }
-
-      setMode('running');
-      setBlocks((previous) => [
-        ...previous,
-        {
-          type: 'command',
-          id: blockId,
-          input: input,
-          commandId: command.id,
-        },
-      ]);
-    }
-    catch (error) {
-      if (error instanceof Error) {
-        const { message } = error;
-
-        setBlocks((previous) => {
-          return [
-            ...previous,
-            {
-              type: 'error',
-              id: blockId,
-              input: input,
-              message: message,
-            },
-          ];
-        });
-
-        return;
-      }
-
-      // Re-throw the error if
-      // it cannot be handled
-      throw error;
-    }
-    finally {
-      // Reset the input value once the
-      // block as been added to state
-      setInputValue('');
-    }
-  };
+  const {
+    mode,
+    blocks,
+    inputValue,
+    serverStatus,
+    apiStatus,
+    loginStatus,
+    config,
+    credentials,
+    setInputValue,
+    executeInput,
+  } = useApp();
 
   /**
    * Used to monitor the user input and take
    * acton based on the keys pressed
    */
   useInput((_, key) => {
-    // If the user pressed the escape key then
-    // set the app mode back to `idle`
-    if (key.escape === true) {
-      setMode('idle');
-    }
-
     if (key.return === true) {
       const trimmed = inputValue.trim();
 
       // Execute the command only if there
       // is an input to execute
       if (trimmed !== '') {
-        _executeInput(trimmed);
+        executeInput(trimmed);
       }
     }
   });
@@ -117,29 +57,36 @@ const App: FunctionComponent = (): ReactElement => {
       <Header />
       <Box
         flexDirection="column"
-        rowGap={1}
         marginX={1}
+        rowGap={1}
       >
-        <Text color={colours.lightGrey}>
-          Run
-          <Text
-            color={colours.purple}
-            bold={true}
-          >
-            {' /init '}
-          </Text>
-          to connect to a Homebridge server
-        </Text>
-        <Text color={colours.lightGrey}>
-          Run
-          <Text
-            color={colours.purple}
-            bold={true}
-          >
-            {' /login '}
-          </Text>
-          to login to Homebridge
-        </Text>
+        <SetupInfo config={config} />
+        {
+          (config != null) && (
+            <>
+              {
+                (serverStatus != null) && (
+                  <ServerInfo
+                    status={serverStatus}
+                    config={config}
+                  />
+                )
+              }
+              {
+                (apiStatus != null) && (
+                  <ApiInfo
+                    status={apiStatus}
+                    config={config}
+                  />
+                )
+              }
+            </>
+          )
+        }
+        <LoginInfo
+          status={loginStatus}
+          credentials={credentials}
+        />
       </Box>
       {
         (blocks.length > 0) && (
