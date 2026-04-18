f2f49d3620bdd521979ea5a304385547cb357764
Liam Hales <liam.hales96@gmail.com>
2026-04-18 14:37:41 +0100

    Updated use of `List` component and removed unused item arrays

--- src/commands/server/info/serverInfoOutput.tsx
+++ src/commands/server/info/serverInfoOutput.tsx
@@ -1,9 +1,8 @@
-import { FunctionComponent, ReactElement, useEffect, useMemo, useState } from 'react';
+import { FunctionComponent, ReactElement, useEffect, useState } from 'react';
 import { Box } from 'ink';
 import { useApiClient } from '../../../hooks/index.js';
 import { NodejsInfo, ServerInfo } from '../../../api/types.js';
 import { List, Loader } from '../../../components/index.js';
-import { ListItem } from '../../../components/types.js';
 
 /**
  * The output component rendered when
@@ -30,148 +29,6 @@ const ServerInfoOutput: FunctionComponent = (): ReactElement => {
     })();
   }, [client, setServerInfo, setNodejsInfo]);
 
-  /**
-   * The list items for
-   * the system list
-   */
-  const systemItems = useMemo<ListItem[]>(() => {
-    if (serverInfo == null) {
-      return [];
-    }
-
-    return [
-      {
-        name: 'service user',
-        value: serverInfo.serviceUser,
-      },
-      {
-        name: 'config path',
-        value: serverInfo.homebridgeConfigJsonPath,
-      },
-      {
-        name: 'storage path',
-        value: serverInfo.homebridgeStoragePath,
-      },
-    ];
-  }, [serverInfo]);
-
-  /**
-   * The list items for the
-   * operating system list
-   */
-  const osItems = useMemo<ListItem[]>(() => {
-    if (serverInfo == null) {
-      return [];
-    }
-
-    const { os } = serverInfo;
-    return [
-      {
-        name: 'platform',
-        value: os.platform,
-      },
-      {
-        name: 'distribution',
-        value: os.distro,
-      },
-      {
-        name: 'kernal',
-        value: os.kernel,
-      },
-      {
-        name: 'architecture',
-        value: os.arch,
-      },
-      {
-        name: 'hostname',
-        value: os.hostname,
-      },
-    ];
-  }, [serverInfo]);
-
-  /**
-   * The list items for
-   * the network list
-   */
-  const networkItems = useMemo<ListItem[]>(() => {
-    if (serverInfo == null) {
-      return [];
-    }
-
-    const { network } = serverInfo;
-    return [
-      {
-        name: 'interface',
-        value: network.iface,
-      },
-      {
-        name: 'ipv4 address',
-        value: network.ip4,
-      },
-      {
-        name: 'ipv4 subnet',
-        value: network.ip4subnet,
-      },
-      {
-        name: 'ipv6 address',
-        value: network.ip6,
-      },
-      {
-        name: 'ipv6 subnet',
-        value: network.ip6subnet,
-      },
-      {
-        name: 'mac address',
-        value: network.mac,
-      },
-      {
-        name: 'connection',
-        value: network.type,
-      },
-      {
-        name: 'duplex',
-        value: network.duplex,
-      },
-      {
-        name: 'speed',
-        value: `${network.speed}`,
-      },
-      {
-        name: 'dns suffix',
-        value: network.dnsSuffix,
-      },
-    ];
-  }, [serverInfo]);
-
-  /**
-   * The list items for
-   * the Node.js list
-   */
-  const nodejsItems = useMemo<ListItem[]>(() => {
-    if (nodejsInfo == null) {
-      return [];
-    }
-
-    return [
-      {
-        name: 'current version',
-        value: nodejsInfo.currentVersion,
-      },
-      {
-        name: 'latest version',
-        value: nodejsInfo.latestVersion,
-      },
-      {
-        name: 'install path',
-        value: nodejsInfo.installPath,
-      },
-      {
-        name: 'npm version',
-        value: nodejsInfo.npmVersion,
-      },
-    ];
-  }, [nodejsInfo]);
-
   return (
     <>
       {
@@ -197,19 +54,19 @@ const ServerInfoOutput: FunctionComponent = (): ReactElement => {
           >
             <List
               title="System"
-              items={systemItems}
+              data={serverInfo.system}
             />
             <List
               title="Operating System"
-              items={osItems}
+              data={serverInfo.os}
             />
             <List
               title="Network"
-              items={networkItems}
+              data={serverInfo.network}
             />
             <List
               title="Node.js"
-              items={nodejsItems}
+              data={nodejsInfo}
             />
           </Box>
         )
