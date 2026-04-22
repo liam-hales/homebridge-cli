958cfa7d0bbc5518980f7699639c6a61703c5e77
Liam Hales <liam.hales96@gmail.com>
2026-04-22 20:26:39 +0100

    Removed unused `PrimitiveObject` type

--- src/types.ts
+++ src/types.ts
@@ -31,14 +31,6 @@ export type LoginStatus = 'authenticated' | 'failed';
 export type Block = ICommandBlock | IErrorBlock;
 
 /**
- * Used to create an object of primitive
- * values `string | number | boolean`
- */
-export type PrimitiveObject<T> = {
-  readonly [K in keyof T]: string | number | boolean;
-};
-
-/**
  * Used to create a map type from a
  * given object type and index key
  *
