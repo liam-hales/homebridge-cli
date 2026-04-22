f00ff882cd75d34661d21803c64f8ab5ca7cfd75
Liam Hales <liam.hales96@gmail.com>
2026-04-22 20:19:28 +0100

    Updated `date.ts` file to export `Date` type and `isDate` type guard

--- src/date.ts
+++ src/date.ts
@@ -1,4 +1,4 @@
-import dayjs from 'dayjs';
+import dayjs, { type Dayjs as Date } from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
 import timezone from 'dayjs/plugin/timezone.js';
 import relativeTime from 'dayjs/plugin/relativeTime.js';
@@ -9,4 +9,18 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(relativeTime);
 
+/**
+ * Used to check if a given
+ * value is a `Date` object
+ *
+ * @param value The value to check
+ */
+const isDate = (value: unknown): value is Date => {
+  return dayjs.isDayjs(value);
+};
+
 export default dayjs;
+export {
+  Date,
+  isDate,
+};
