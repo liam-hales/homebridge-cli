8135cc0999e0cffdb9259249aba31a685c9dd2fa
Liam Hales <liam.hales96@gmail.com>
2026-04-22 20:20:42 +0100

    Added `TextListData` and `TableItem` types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,3 +1,17 @@
+import type { Date } from '../date.js';
+
+/**
+ * Describes the list data used for
+ * the `TextList` component
+ */
+export type TextListData = Record<string, string | number | boolean | Date>;
+
+/**
+ * Describes the table item used for
+ * the `Table` component
+ */
+export type TableItem = Record<string, string | number | boolean | Date>;
+
 /**
  * Describes a keybinding used for
  * the `Keybindings` component
