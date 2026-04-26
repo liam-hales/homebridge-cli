a0a949492fab0d73bb392b160e78a7332a0ca151
Liam Hales <liam.hales96@gmail.com>
2026-04-26 18:53:34 +0100

    Added `useQuery` hook which executes a given function used to fetch data and handles the loading, data and error state

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,9 @@
 import useApp from './useApp.hook.js';
 import useApiClient from './useApiClient.hook.js';
+import useQuery from './useQuery.hook.js';
 
 export {
   useApp,
   useApiClient,
+  useQuery,
 };
