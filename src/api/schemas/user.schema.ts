c9381952e414c5d6e49cb4c243c048e09e97fb47
Liam Hales <liam.hales96@gmail.com>
2026-04-27 16:29:17 +0100

    Removed unused `transform` function from `userSchema`

--- src/api/schemas/user.schema.ts
+++ src/api/schemas/user.schema.ts
@@ -4,20 +4,11 @@ import { z } from 'zod';
  * The schema used to describe and transform the user data
  * returned from the `GET /api/users` endpoint
  */
-const userSchema = z
-  .object({
-    id: z.number(),
-    name: z.string(),
-    username: z.string(),
-    admin: z.boolean(),
-  })
-  .transform((data) => {
-    return {
-      id: data.id,
-      name: data.name,
-      username: data.username,
-      admin: data.admin,
-    };
-  });
+const userSchema = z.object({
+  id: z.number(),
+  name: z.string(),
+  username: z.string(),
+  admin: z.boolean(),
+});
 
 export default userSchema;
