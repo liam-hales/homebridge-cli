34c03e3b2b2f98478cc7a6b3c410406742d3e038
Liam Hales <liam.hales96@gmail.com>
2026-04-28 19:07:26 +0100

    Added logic to enable `duration` plugin for `dayjs`

--- src/date.ts
+++ src/date.ts
@@ -2,12 +2,14 @@ import dayjs, { type Dayjs as Date } from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
 import timezone from 'dayjs/plugin/timezone.js';
 import relativeTime from 'dayjs/plugin/relativeTime.js';
+import duration from 'dayjs/plugin/duration.js';
 
-// Configure Day.js to use the UTC,
-// timezone and relative time plugins
+// Configure Day.js to use the UTC, timezone,
+// relative time and duration plugins
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(relativeTime);
+dayjs.extend(duration);
 
 /**
  * Used to check if a given
