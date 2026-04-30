d19b5da655b1395ac98fa992f78a48091bb3390d
Liam Hales <liam.hales96@gmail.com>
2026-04-30 22:09:28 +0100

    Updated `CommandInput` to use `Picker` component for the command list

--- src/components/commandInput.tsx
+++ src/components/commandInput.tsx
@@ -1,8 +1,9 @@
-import { FunctionComponent, ReactElement, useEffect, useMemo, useState } from 'react';
-import { Box, Text, useInput } from 'ink';
+import { FunctionComponent, ReactElement, useMemo } from 'react';
+import { Box, useInput } from 'ink';
 import { colours } from '../constants.js';
 import { commands } from '../commands/index.js';
-import { Keybindings } from './index.js';
+import { Keybindings, Picker } from './index.js';
+import { PickerItem } from './types.js';
 import TextInput from 'ink-text-input';
 
 /**
@@ -21,51 +22,21 @@ interface Props {
  * @returns The `CommandInput` component
  */
 const CommandInput: FunctionComponent<Props> = ({ value, onChange }): ReactElement<Props> => {
-  const [listIndex, setListIndex] = useState<number>(0);
-
   /**
-   * Used to calculate the filtered list of commands
-   * based on the current input value
+   * Used to calculate the command picker
+   * items based on the current input value
    */
-  const filteredCommands = useMemo(() => {
+  const commandPickerItems = useMemo<PickerItem[]>(() => {
     return commands
       .filter(({ invoke }) => invoke.includes(value))
-      .sort((a, b) => a.invoke.localeCompare(b.invoke));
+      .sort((a, b) => a.invoke.localeCompare(b.invoke))
+      .map((command) => {
+        return {
+          name: command.invoke,
+          description: command.description,
+        };
+      });
   }, [value]);
-
-  /**
-   * Used to calculate the command window which
-   * consists of the window commands and the index
-   */
-  const [windowCommands, windowIndex] = useMemo(() => {
-    const windowSize = 6;
-    const windowScrollPoint = 3;
-
-    // Calculate the max and scroll offsets for the command
-    // window using the filtered commands and list index
-    const maxOffset = Math.max(0, filteredCommands.length - windowSize);
-    const scrollOffset = Math.min(
-      Math.max(0, listIndex - windowScrollPoint),
-      maxOffset,
-    );
-
-    // Calculate the command window and the window
-    // index which will be used to render the window
-    const commandWindow = filteredCommands.slice(scrollOffset, scrollOffset + windowSize);
-    const windowIndex = listIndex - scrollOffset;
-
-    return [
-      commandWindow,
-      windowIndex,
-    ];
-  }, [filteredCommands, listIndex]);
-
-  /**
-   * Used to reset the list index
-   * state when the value changes
-   */
-  // eslint-disable-next-line react-hooks/set-state-in-effect
-  useEffect(() => setListIndex(0), [value]);
 
   /**
    * Used to monitor the user input and take
@@ -76,38 +47,6 @@ const CommandInput: FunctionComponent<Props> = ({ value, onChange }): ReactEleme
     // clear the input value
     if (key.escape === true) {
       onChange('');
-    }
-
-    if (key.tab === true && value.startsWith('/') === true) {
-      const { invoke } = filteredCommands[listIndex];
-
-      // Call `onChange` with the
-      // command invoke value
-      onChange(invoke);
-    }
-
-    if (key.upArrow === true) {
-      const nextIndex = listIndex - 1;
-
-      // Set the new list index based on the next one to be set
-      // If the user has reached the start of the list then send them to the end
-      setListIndex(
-        (nextIndex >= 0)
-          ? nextIndex
-          : filteredCommands.length - 1,
-      );
-    }
-
-    if (key.downArrow === true) {
-      const nextIndex = listIndex + 1;
-
-      // Set the new list index based on the next one to be set
-      // If the user has reached the end of the list then send them bac to the start
-      setListIndex(
-        (nextIndex < filteredCommands.length)
-          ? nextIndex
-          : 0,
-      );
     }
   });
 
@@ -139,61 +78,10 @@ const CommandInput: FunctionComponent<Props> = ({ value, onChange }): ReactEleme
         {
           (value.startsWith('/') === true) && (
             <>
-              <Box
-                height={6}
-                flexDirection="row"
-              >
-                <Box
-                  flexDirection="column"
-                  width={24}
-                >
-                  {
-                    // Map the commands into text components
-                    // used to render the command invoke values
-                    windowCommands.map((command, index) => {
-                      const { invoke } = command;
-
-                      return (
-                        <Text
-                          key={`command-invoke-${invoke}`}
-                          color={
-                            (index === windowIndex)
-                              ? colours.purple
-                              : colours.white
-                          }
-                        >
-                          {invoke}
-                        </Text>
-                      );
-                    })
-                  }
-                </Box>
-                <Box
-                  flexDirection="column"
-                  flexGrow={1}
-                >
-                  {
-                    // Map the commands into text components
-                    // used to render the command descriptions
-                    windowCommands.map((command, index) => {
-                      const { description } = command;
-
-                      return (
-                        <Text
-                          key={`command-description-${description}`}
-                          color={
-                            (index === windowIndex)
-                              ? colours.purple
-                              : colours.lightGrey
-                          }
-                        >
-                          {description}
-                        </Text>
-                      );
-                    })
-                  }
-                </Box>
-              </Box>
+              <Picker
+                items={commandPickerItems}
+                onSelect={(item) => onChange(item.name)}
+              />
               <Keybindings bindings={[
                 {
                   key: '↑/↓',
