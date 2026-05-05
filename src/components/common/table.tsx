d25ccb23e89c27fbf481da5ef587609f2ba6d1de
Liam Hales <liam.hales96@gmail.com>
2026-05-05 18:02:28 +0100

    Updated `Table` component to support custom padding and row selection

--- src/components/common/table.tsx
+++ src/components/common/table.tsx
@@ -13,25 +13,53 @@ import { Keybindings, Pagination } from '../index.js';
 interface Props<T extends TableItem> {
   readonly items: T[];
   readonly pageSize?: number;
+  readonly padding?: number;
   readonly format?: ValueFormatters<T>;
+  readonly isSelectable?: boolean;
+  readonly onSelect?: (item: T) => void;
 }
 
 /**
- * Used to render a paginated
- * table of items
+ * Used to render a paginated table of
+ * items with optional row selection
  *
  * - Generic type `T` for the item
  *
  * @param props The component props
  * @returns The `Table` component
  */
-const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props<T>): ReactElement<Props<T>> => {
-  const [page, setPage] = useState<number>(1);
+const Table = <T extends TableItem>(props: Props<T>): ReactElement<Props<T>> => {
+  const {
+    items,
+    pageSize = 10,
+    padding = 4,
+    format,
+    isSelectable = false,
+    onSelect,
+  } = props;
 
-  // Define the padding and calculate the total number
-  // of pages using the items length and page size
-  const padding = 4;
-  const totalPages = Math.ceil(items.length / pageSize);
+  const [position, setPosition] = useState<number>(0);
+
+  /**
+   * Used to calculate the cursor index, page and
+   * total page count when the position changes
+   */
+  const [cursorIndex, page, totalPages] = useMemo(() => {
+    // Define the page and cursor index using
+    // the current position and page size
+    const page = Math.floor(position / pageSize) + 1;
+    const cursorIndex = position % pageSize;
+
+    // Calculate the total number of pages
+    // using the items length and page size
+    const totalPages = Math.ceil(items.length / pageSize);
+
+    return [
+      cursorIndex,
+      page,
+      totalPages,
+    ];
+  }, [position]);
 
   /**
    * Used to calculate the headers, rows
@@ -74,7 +102,7 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
 
             // Check if there is a value formatter for
             // the key and if so use it to transform the value
-            const formatter = format[key];
+            const formatter = format?.[key];
             return (formatter != null) ? formatter(value) : value.toString();
           });
       });
@@ -96,7 +124,7 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
       rows,
       widths,
     ];
-  }, [items, page]);
+  }, [page]);
 
   // Calculate the Build the separator line which spans all columns
   // with a `+` at each column intersection and edge
@@ -107,28 +135,56 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
    * acton based on the keys pressed
    */
   useInput((_, key) => {
-    if (key.rightArrow === true) {
-      const nextPage = page + 1;
+    // Only run the logic for selecting
+    // rows if the table is selectable
+    if (isSelectable === true) {
+      if (key.return === true && items.length > 0) {
+        const item = items[position];
 
-      // If the user has reached the last page
-      // then send them to the first
-      setPage(
-        (nextPage <= totalPages)
-          ? nextPage
-          : 1,
-      );
+        // Call `onSelect` with the
+        // currently focussed item
+        onSelect?.(item);
+      }
+
+      if (key.upArrow === true) {
+        const nextPosition = position - 1;
+
+        // If the user has reached the first row
+        // then send them to the last
+        setPosition((nextPosition >= 0)
+          ? nextPosition
+          : items.length - 1,
+        );
+      }
+
+      if (key.downArrow === true) {
+        const nextPosition = position + 1;
+
+        // If the user has reached the last row
+        // then send them to the first
+        setPosition((nextPosition < items.length)
+          ? nextPosition
+          : 0,
+        );
+      }
     }
 
     if (key.leftArrow === true) {
       const nextPage = page - 1;
+      const targetPage = (nextPage >= 1) ? nextPage : totalPages;
 
-      // If the user has reached the first page
-      // then send them to the last
-      setPage(
-        (nextPage >= 1)
-          ? nextPage
-          : totalPages,
-      );
+      // Send the user to the target page
+      // by setting the correct position
+      setPosition((targetPage - 1) * pageSize);
+    }
+
+    if (key.rightArrow === true) {
+      const nextPage = page + 1;
+      const targetPage = (nextPage <= totalPages) ? nextPage : 1;
+
+      // Send the user to the target page
+      // by setting the correct position
+      setPosition((targetPage - 1) * pageSize);
     }
   });
 
@@ -138,7 +194,10 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
       alignItems="flex-start"
       rowGap={1}
     >
-      <Box flexDirection="column">
+      <Box
+        flexDirection="column"
+        marginLeft={(isSelectable === true) ? 2 : 0}
+      >
         <Text color={colours.darkGrey}>
           {separator}
         </Text>
@@ -148,6 +207,12 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
           </Text>
           {
             headers.map((header, index) => {
+              // Generate the header text by padding out the
+              // value using the padding and column width
+              const headerText = header
+                .padStart(header.length + (padding / 2))
+                .padEnd(widths[index] + padding);
+
               return (
                 <Box
                   key={`table-header-cell-${header}`}
@@ -157,11 +222,7 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
                     color={colours.purple}
                     bold={true}
                   >
-                    {
-                      header
-                        .padStart(header.length + (padding / 2))
-                        .padEnd(widths[index] + padding)
-                    }
+                    {headerText}
                   </Text>
                   <Text color={colours.darkGrey}>
                     |
@@ -177,62 +238,124 @@ const Table = <T extends TableItem>({ items, pageSize = 10, format = {} }: Props
       </Box>
       <Box flexDirection="column">
         {
-          rows.map((row, index) => {
+          rows.map((row, rowIndex) => {
+            const isFocussed = (isSelectable === true && rowIndex === cursorIndex);
             return (
-              <Box key={`table-row-${index}`}>
-                <Box flexDirection="row">
-                  <Text color={colours.darkGrey}>
-                    |
-                  </Text>
-                  {
-                    row.map((cell, index) => {
-                      return (
-                        <Box
-                          key={`table-row-cell-${cell}`}
-                          flexDirection="row"
+              <Box
+                key={`table-row-${rowIndex}`}
+                flexDirection="row"
+              >
+                {
+                  (isSelectable === true) && (
+                    <Box
+                      width={2}
+                      flexDirection="column"
+                    >
+                      {
+                        (rowIndex === cursorIndex) && (
+                          <Text
+                            color={colours.purple}
+                            bold={true}
+                          >
+                            ⟶
+                          </Text>
+                        )
+                      }
+                    </Box>
+                  )
+                }
+                {
+                  (isFocussed === true)
+                    ? (
+                        <Text
+                          color={colours.white}
+                          backgroundColor={colours.purple}
                         >
-                          <Text color={colours.lightGrey}>
-                            {
-                              cell
-                                .padStart(cell.length + (padding / 2))
-                                .padEnd(widths[index] + padding)
-                            }
-                          </Text>
-                          <Text color={colours.darkGrey}>
-                            |
-                          </Text>
-                        </Box>
-                      );
-                    })
-                  }
-                </Box>
+                          |
+                        </Text>
+                      )
+                    : (
+                        <Text color={colours.darkGrey}>
+                          |
+                        </Text>
+                      )
+                }
+                {
+                  row.map((cell, cellIndex) => {
+                    // Generate the cell text by padding out the
+                    // value using the padding and column width
+                    const cellText = cell
+                      .padStart(cell.length + (padding / 2))
+                      .padEnd(widths[cellIndex] + padding);
+
+                    return (
+                      <Box
+                        key={`table-row-cell-${cell}`}
+                        flexDirection="row"
+                      >
+                        {
+                          (isFocussed === true)
+                            ? (
+                                <>
+                                  <Text
+                                    color={colours.white}
+                                    backgroundColor={colours.purple}
+                                  >
+                                    {`${cellText}|`}
+                                  </Text>
+                                </>
+                              )
+                            : (
+                                <>
+                                  <Text color={colours.lightGrey}>
+                                    {cellText}
+                                  </Text>
+                                  <Text color={colours.darkGrey}>
+                                    |
+                                  </Text>
+                                </>
+                              )
+                        }
+                      </Box>
+                    );
+                  })
+                }
               </Box>
             );
           })
         }
       </Box>
-      {
-        (totalPages > 1) && (
-          <Box
-            width={separator.length}
-            flexDirection="row"
-            justifyContent="space-between"
-            paddingX={1}
-          >
-            <Keybindings bindings={[
-              {
+      <Box
+        width={separator.length}
+        flexDirection="row"
+        justifyContent="space-between"
+        marginLeft={(isSelectable === true) ? 2 : 0}
+        paddingX={1}
+      >
+        <Keybindings bindings={[
+          ...(isSelectable === true)
+            ? [{
+                key: '↑/↓',
+                action: 'to select',
+              }]
+            : [],
+          ...(totalPages > 1)
+            ? [{
                 key: '←/→',
                 action: 'to page',
-              },
-            ]}
-            />
+              }]
+            : [],
+        ]}
+        />
+        {
+          (totalPages > 1) && (
             <Pagination
               page={page}
               totalPages={totalPages}
             />
-          </Box>
-        )
-      }
+          )
+        }
+      </Box>
     </Box>
   );
 };
